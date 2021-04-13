1019d519252134ec4fc960a83346b42c304cddeb
Susheel Kumar Pogaku <pogaku9@users.noreply.github.com>
2021-04-13 14:02:08 -0700

    test sidebars generation script (#3)
    * test sidebars generation script
    
    * test sidebars generation script
    
    * test sidebars generation script
    
    * test sidebars generation script

--- generate_sidebars.sh
+++ generate_sidebars.sh
@@ -3,10 +3,9 @@
 rm -rf ./static/temp/*
 find docs/ -name "_sidebar.json" | while IFS= read -r NAME; do
     actualPath="${NAME/\/\///}"
-    fullPath="${NAME/docs\/\//}"
+    fullPath="${actualPath/docs\//}"
     relativePath="${fullPath/_sidebar.json/}"
     tempFilePath="./static/temp/${NAME//\//_}"
-    echo "${actualPath}"
     jq --arg relativePath "${relativePath}" '(.. | .id?) |= if . then $relativePath + . else empty end | (.. | .items?) |= if . then (.[] |= if type=="string" then $relativePath + . else . end) else empty end' ${actualPath} > ${tempFilePath}
 done
 jq -rs 'reduce .[] as $item ({}; . * $item)' ./static/temp/*.json > ./static/combined_sidebars.json
